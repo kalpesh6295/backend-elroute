c83b671c5b2f72c493c71f3b82261727ff5d79d3
unknown <raj.abhisingh17@gmail.com>
2018-10-08 15:33:49 +0530

    Checking repository

--- mongoose/mongoose-connect.js
+++ mongoose/mongoose-connect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 // const env=require('./../config/env.js');
 
-mongoose.connect('mongodb://kalpesh6295:kp6295.iitb@ds223763.mlab.com:23763/tradifier');
+// mongoose.connect('mongodb://kalpesh6295:kp6295.iitb@ds223763.mlab.com:23763/tradifier',{useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/Tradifier',{useNewUrlParser:true});
 
 module.exports = {mongoose};
