cf5004ff03a39d91d80525fcd908bcf6fc20b9f1
vivek raj <vivekraj776@gmail.com>
2018-12-06 15:14:08 +0530

    bookmark save

--- Routes/Bookmarksave.js
+++ Routes/Bookmarksave.js
@@ -7,39 +7,31 @@ const { companyModel } = require('./../Modals/companyModel.js');
 var json2csv = require('json2csv').parse;
 var fs = require('fs');
 
-router.get('/',authenticate,(request,response)=>{
+
+router.get('/',authenticate,async (request,response)=>{
     //console.log(request.user.id);
-    userModel.find({_id:request.user.id}).then((items)=>{
-        console.log(items[0].bookmarks.company.length);
-        var k=0;
-    for(var i=0;i<items[0].bookmarks.company.length;i++);
-            {
-                
 
-            // console.log(items[0].bookmarks.company[i]); 
-            // companyModel.find({ _id: items[0].bookmarks.company[i] }).then((docs) => {
-            //     console.log(docs[0].location,docs[0].companyName,docs[0].website,docs[0].industry,docs[0].email);
-            //     console.log(i);
-            //     i++;
-            //     })
-            }
-    //    var fields=['bookmarks'];
-    //     var csv = json2csv({ data: items[0].bookmarks,fields:fields });
-    //      console.log(csv);
-    //     fs.writeFile('file.csv', csv, function (err) {
-    //         if (err) throw err;
-    //         console.log('file saved');
-    //     });
-    }) 
-    
-        
+    var result=[];
 
-            // console.log(club)
-            // var temp = items[0];
-            // items[0] = items[items.length - 1];
-            // items[items.length - 1] = temp;
+    var temp=await userModel.find({_id:request.user.id});
+    console.log(temp[0].bookmarks.company.length);
 
-            // var fields = ['hostname', 'user', 'db', 'elevated', 'superuser'];
+    for(var i=0;i<temp[0].bookmarks.company.length;i++)
+    {
+        var company=await companyModel.find({_id:temp[0].bookmarks.company[i]});
+        result.push(company[0].companyName);
+        result.push(company[0].location);
+        result.push(company[0].website);
+        result.push(company[0].email);
+    }
+
+    var fields = ['companyName','location','website','email'];
+    var csv = json2csv({ data: result, fields: fields });
+    console.log(csv);
+    await fs.writeFile('file.csv', csv, function (err) {
+        if (err) throw err;
+        console.log('file saved');
+    });
             
 });
 
