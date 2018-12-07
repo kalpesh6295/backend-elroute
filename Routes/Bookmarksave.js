852de3667bd8ff86459d6722984208ddf1500fd4
vivek raj <vivekraj776@gmail.com>
2018-12-07 14:50:27 +0530

    Bookmarksave completed

--- Routes/Bookmarksave.js
+++ Routes/Bookmarksave.js
@@ -5,34 +5,27 @@ const { userModel } = require('./../Modals/userModel.js');
 const { authenticate } = require('./../middleware/authenticate.js');
 const { companyModel } = require('./../Modals/companyModel.js');
 var json2csv = require('json2csv').parse;
+var jsonexport=require('jsonexport');
 var fs = require('fs');
-
+const ObjectsToCsv = require('objects-to-csv');
 
 router.get('/',authenticate,async (request,response)=>{
-    //console.log(request.user.id);
-
     var result=[];
 
     var temp=await userModel.find({_id:request.user.id});
-    console.log(temp[0].bookmarks.company.length);
-
     for(var i=0;i<temp[0].bookmarks.company.length;i++)
     {
         var company=await companyModel.find({_id:temp[0].bookmarks.company[i]});
-        result.push(company[0].companyName);
-        result.push(company[0].location);
-        result.push(company[0].website);
-        result.push(company[0].email);
+        
+        result.push({companyName : company[0].companyName,
+        Location : company[0].location,
+        website : company[0].website,
+        email : company[0].email});
     }
-
-    var fields = ['companyName','location','website','email'];
-    var csv = json2csv({ data: result, fields: fields });
-    console.log(csv);
-    await fs.writeFile('file.csv', csv, function (err) {
-        if (err) throw err;
-        console.log('file saved');
-    });
-            
+    
+            let csv = new ObjectsToCsv(result);
+            await csv.toDisk('./test.csv');
+            fs.createReadStream('./test.csv').pipe(response);
 });
 
 module.exports=router;
