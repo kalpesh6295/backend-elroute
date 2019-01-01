5f0f4a480cce1db12b6620871907b8b3da089476
avinashk29 <akasliwal1998@gmail.com>
2019-01-01 17:34:24 +0530

    playground product search added

--- Playground/playground.js
+++ Playground/playground.js
@@ -32,22 +32,22 @@ console.log(diff/(1000*60*60*24));
 //     console.log(results[0].dateDifference/(1000*60*60*24))
 // })
 
-//dummy data for products --> Schema:companies on www.mocakroo.com
-// client.generate({
-//     count: 500,
-//     schema: 'product'
-// }).then((records)=>{
-//     // console.log(records);
-//     for(var i=0;i<records.length;i++){
-//         var company = new serviceModel(records[i]);
-//         // console.log(company);
-//         company.save().then((result)=>{
-//             console.log('Result after saving is',result);
-//         })
-//     }
-// }).catch((e)=>{
-//     console.log('Error is',e);
-// });
+// dummy data for products --> Schema:companies on www.mocakroo.com
+client.generate({
+    count: 500,
+    schema: 'product'
+}).then((records)=>{
+    // console.log(records);
+    for(var i=0;i<records.length;i++){
+        var product = new productModel(records[i]);
+        // console.log(company);
+        product.save().then((result)=>{
+            console.log('Result after saving is',result);
+        })
+    }
+}).catch((e)=>{
+    console.log('Error is',e);
+});
 
 
 
