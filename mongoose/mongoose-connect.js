ab3078ac0bda5892a8e6e547ecfb9b0e354d0820
unknown <raj.abhisingh17@gmail.com>
2019-01-02 15:42:27 +0530

    env updated

--- mongoose/mongoose-connect.js
+++ mongoose/mongoose-connect.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Tradifier',{useNewUrlParser:true});         //localhost where moongoose is stored
-mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false);
-module.exports = {mongoose};
-
-
-
-
-// var mongoose = require('mongoose');
-
-// var mongoDB = "mongodb://kalpesh:kp6295.iitb@ds145574.mlab.com:45574/tradifier";
-
-// mongoose.connect(mongoDB, 
-//     {useNewUrlParser:true}
-// );
-
-// var db = mongoose.connection;
-
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// mongoose.connect('mongodb://localhost:27017/Tradifier',{useNewUrlParser:true});         //localhost where moongoose is stored
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useFindAndModify', false);
 // module.exports = {mongoose};
+
+
+
+const env = require('./env/env.js');
+var mongoose = require('mongoose');
+
+var mongoDB = env.mongoDB;
+
+mongoose.connect(mongoDB, 
+    {useNewUrlParser:true}
+);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+module.exports = {mongoose};
